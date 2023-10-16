3bda1e4b96b01db5c2a0685a6c072aab86239a51
Daniel Penelva <d4n.andrade@gmail.com>
2023-10-16 20:21:14 -0300

    Criando o método HTTP Get de acesso a página do registro de produtos (ProdutoController) + criando a página HTML de registro de produto (produto_formulario) + Criando o método HTTP Post para criar produto

---
+++ src/main/java/com/sistema/inventarioapp/produto/ProdutoController.java
@@ -0,0 +1,39 @@
+package com.sistema.inventarioapp.produto;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sistema.inventarioapp.categoria.Categoria;
+import com.sistema.inventarioapp.categoria.CategoriaRepository;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+public class ProdutoController {
+
+    private ProdutoRepository produtoRepository;
+
+    private CategoriaRepository categoriaRepository;
+
+    // http://localhost:8080/produtos/novo
+    @GetMapping("/produtos/novo")
+    public String mostrarFormularioDeNovaProduto(Model model){
+        
+        List<Categoria> listaCategorias = categoriaRepository.findAll();
+
+        model.addAttribute("produto", new Produto());
+        model.addAttribute("listaCategorias", listaCategorias);
+
+        return "produto_formulario";
+    }
+
+    public String criarProduto(Produto produto){
+        produtoRepository.save(produto);
+        return "redirect:/";
+    }
+    
+}
