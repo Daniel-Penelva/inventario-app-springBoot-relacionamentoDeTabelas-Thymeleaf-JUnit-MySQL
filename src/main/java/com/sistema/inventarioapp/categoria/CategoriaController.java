b3ac633b0e0f9b124b88cf242cdb66ac9b8dc17d
Daniel Penelva <d4n.andrade@gmail.com>
2023-10-16 19:30:13 -0300

    Criando método HTTP Get para criar o acesso a página de Registro de Categoria + criando página de Registro de categoria + criando método HTTP Post para criar categoria

--- src/main/java/com/sistema/inventarioapp/categoria/CategoriaController.java
+++ src/main/java/com/sistema/inventarioapp/categoria/CategoriaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.AllArgsConstructor;
 
@@ -14,11 +15,26 @@ public class CategoriaController {
 
     private CategoriaRepository categoriaRepository;
 
+
+    // http://localhost:8080/categorias
     @GetMapping("/categorias")
     public String listarCategorias(Model model) {
         List<Categoria> listaCategorias = categoriaRepository.findAll();
         model.addAttribute("listaCategorias", listaCategorias);
         return "categorias";
+    }
+
+    // http://localhost:8080/categorias/novo
+    @GetMapping("/categorias/novo")
+    public String mostrarFormularioDeNovaCategoria(Model model){
+        model.addAttribute("categoria", new Categoria());
+        return "categoria_formulario";
+    }
+
+    @PostMapping("/categorias/guardar")
+    public String criarCategoria(Categoria categoria){
+        categoriaRepository.save(categoria);
+        return "redirect:/categorias";
     }
 
 }
