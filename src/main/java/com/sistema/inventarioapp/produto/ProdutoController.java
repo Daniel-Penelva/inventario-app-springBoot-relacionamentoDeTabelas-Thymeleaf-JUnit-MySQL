ab7506e3916471c676dee57f6530494a311d0e49
Daniel Penelva <d4n.andrade@gmail.com>
2023-10-16 21:14:21 -0300

    Criando um camplo select para selecionar a categoria + criando o método HTTP GET para listar o produto + criando a página HTML para listar os produtos

--- src/main/java/com/sistema/inventarioapp/produto/ProdutoController.java
+++ src/main/java/com/sistema/inventarioapp/produto/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.sistema.inventarioapp.categoria.Categoria;
 import com.sistema.inventarioapp.categoria.CategoriaRepository;
@@ -31,9 +32,19 @@ public class ProdutoController {
         return "produto_formulario";
     }
 
+    @PostMapping("/produtos/guardar")
     public String criarProduto(Produto produto){
         produtoRepository.save(produto);
         return "redirect:/";
+    }
+
+    @GetMapping("/produtos")
+    public String listarProdutos(Model model){
+
+        List<Produto> listaProdutos = produtoRepository.findAll();
+
+        model.addAttribute("listaProdutos", listaProdutos);
+        return "/produtos";
     }
     
 }
