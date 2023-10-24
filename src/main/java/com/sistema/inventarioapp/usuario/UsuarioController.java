430f3da2c7f22c5f1049ba54b39a4a9bf40e1756
Daniel Penelva <d4n.andrade@gmail.com>
2023-10-24 19:03:46 -0300

    Criando os campos de usuario no index.html + criando UsuarioController e método HTTP GET listar todos usuarios + criando página de lista de usuarios

---
+++ src/main/java/com/sistema/inventarioapp/usuario/UsuarioController.java
@@ -0,0 +1,26 @@
+package com.sistema.inventarioapp.usuario;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsuarioController {
+
+    @Autowired
+    UsuarioRepository usuarioReository;
+
+    @GetMapping("/usuarios")
+    public String listarUsuarios(Model model) {
+
+        List<Usuario> listaUsuarios = usuarioReository.findAll();
+
+        model.addAttribute("listaUsuarios", listaUsuarios);
+
+        return "usuarios";
+    }
+
+}
