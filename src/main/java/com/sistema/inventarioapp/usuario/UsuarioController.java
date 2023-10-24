4b29d55ddb766367b3689e24fe9b5f6d1752926c
Daniel Penelva <d4n.andrade@gmail.com>
2023-10-24 20:18:44 -0300

    Criando o campo editar na página usuario.html + criando o método HTTP GET para editar usuário + alterando formulario usuario

--- src/main/java/com/sistema/inventarioapp/usuario/UsuarioController.java
+++ src/main/java/com/sistema/inventarioapp/usuario/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -44,5 +45,16 @@ public class UsuarioController {
         return "redirect:/usuarios";
     }
 
+    @GetMapping("/usuarios/editar/{id}")
+    public String mostrarFormularioEditarUsuario(@PathVariable("id") Integer id, Model model){
+        
+        Usuario usuario = usuarioReository.findById(id).get();
+        model.addAttribute("usuario", usuario);
 
+        // Recupera a lista de usuario
+        List<Rol> listaRoles = rolReository.findAll();
+        model.addAttribute("listaRoles", listaRoles);
+
+        return "usuario_formulario";
+    }
 }
